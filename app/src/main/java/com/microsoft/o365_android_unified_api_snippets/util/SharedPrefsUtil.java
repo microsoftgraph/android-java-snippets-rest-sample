162d143a522474f334c275c0f63790c83e4168b6
unknown <davech@DAVECHL10.redmond.corp.microsoft.com>
2015-09-10 16:49:36 -0700

    Added auth module code and lots of cleanup work to get it working

---
+++ app/src/main/java/com/microsoft/o365_android_unified_api_snippets/util/SharedPrefsUtil.java
@@ -0,0 +1,56 @@
+/*
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
+*/
+package com.microsoft.o365_android_unified_api_snippets.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.microsoft.aad.adal.AuthenticationResult;
+import com.microsoft.o365_android_unified_api_snippets.application.SnippetApp;
+import com.microsoft.o365_android_unified_api_snippets.inject.AppModule;
+
+public class SharedPrefsUtil {
+
+    public static final String PREF_AUTH_TOKEN = "PREF_AUTH_TOKEN";
+
+    public static SharedPreferences getSharedPreferences() {
+        return SnippetApp.getApp().getSharedPreferences(AppModule.PREFS, Context.MODE_PRIVATE);
+    }
+
+    public static void persistAuthToken(AuthenticationResult result) {
+        setAccessToken(result.getAccessToken());
+    }
+
+    private static void setAccessToken(String accessToken) {
+        getSharedPreferences().edit().putString(PREF_AUTH_TOKEN, accessToken).commit();
+    }
+}
+// *********************************************************
+//
+// Android-REST-API-Explorer, https://github.com/OneNoteDev/Android-REST-API-Explorer
+//
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// *********************************************************
