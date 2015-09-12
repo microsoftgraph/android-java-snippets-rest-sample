3dd04df364ef5d1ba73fa0ff0a910ae75f002ade
Brian Melton <brianmel@microsoft.com>
2015-09-11 17:22:43 -0700

    pkg rename

--- o365-auth/src/main/java/com/microsoft/o365_auth/AzureADModule.java
+++ o365-auth/src/main/java/com/microsoft/o365_auth/AzureADModule.java
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
  *  See full license at the bottom of this file.
  */
-package com.microsoft.O365_auth;
+package com.microsoft.o365_auth;
 
 import android.app.Activity;
 
--- o365-auth/src/main/java/com/microsoft/o365_auth/AzureAppCompatActivity.java
+++ o365-auth/src/main/java/com/microsoft/o365_auth/AzureAppCompatActivity.java
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
  *  See full license at the bottom of this file.
  */
-package com.microsoft.O365_auth;
+package com.microsoft.o365_auth;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
