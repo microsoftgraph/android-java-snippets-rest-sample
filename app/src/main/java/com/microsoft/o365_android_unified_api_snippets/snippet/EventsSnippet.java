e4159ec1ebb18307acd4f4cebdb5c48ddc422a7d
unknown <davech@DAVECHL10.redmond.corp.microsoft.com>
2015-09-16 17:29:55 -0700

    Added create event snippet

--- app/src/main/java/com/microsoft/o365_android_unified_api_snippets/snippet/EventsSnippet.java
+++ app/src/main/java/com/microsoft/o365_android_unified_api_snippets/snippet/EventsSnippet.java
@@ -1,11 +1,25 @@
 package com.microsoft.o365_android_unified_api_snippets.snippet;
 
 import com.microsoft.unifiedapi.service.UnifiedEventsService;
+import com.microsoft.unifiedvos.Envelope;
+import com.microsoft.unifiedvos.EventVO;
+import com.microsoft.unifiedvos.LocationVO;
+
+import java.util.Date;
 
 import retrofit.*;
 import retrofit.Callback;
+import retrofit.mime.TypedString;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
 
 import static com.microsoft.o365_android_unified_api_snippets.R.array.get_user_events;
+import static com.microsoft.o365_android_unified_api_snippets.R.array.create_event;
+import static com.microsoft.o365_android_unified_api_snippets.R.array.update_event;
+import static com.microsoft.o365_android_unified_api_snippets.R.array.delete_event;
 
 
 public abstract class EventsSnippet<Result> extends AbstractSnippet<UnifiedEventsService, Result> {
@@ -26,9 +40,9 @@ public abstract class EventsSnippet<Result> extends AbstractSnippet<UnifiedEvent
                 },
 
                 /*
-                 * POST a new OneNote page in the section picked by the user
-                 * HTTP POST https://www.onenote.com/api/beta/me/notes/sections/{id}/pages
-                 * @see http://dev.onenote.com/docs#/reference/post-pages
+                 * GET all events for the signed in user.
+                 * HTTP POST
+                 * @see https://msdn.microsoft.com/office/office365/HowTo/office-365-unified-api-reference#msg_ref_entityType_Event
                  */
                 new EventsSnippet<Void>(get_user_events) {
 
@@ -43,11 +57,87 @@ public abstract class EventsSnippet<Result> extends AbstractSnippet<UnifiedEvent
                 },
 
                 /*
-                 * POST a new OneNote page in the section picked by the user
-                 * HTTP POST https://www.onenote.com/api/beta/me/notes/sections/{id}/pages
-                 * @see http://dev.onenote.com/docs#/reference/post-pages
+                 * POST Adds an event to the signed-in user\'s calendar.
+                 * HTTP POST https://graph.microsoft.com/beta/me/events
+                 * @see https://msdn.microsoft.com/office/office365/HowTo/office-365-unified-api-reference#msg_ref_entityType_Event
                  */
-                new EventsSnippet<Void>(get_user_events) {
+                new EventsSnippet<Envelope<EventVO>>(create_event) {
+
+                    @Override
+                    public void request(
+                            UnifiedEventsService unifiedEventsService,
+                            retrofit.Callback<Envelope<EventVO>> callback) {
+                        //Create body defining the new event
+                        DateTime start = new DateTime().now();
+                        DateTime end = start.plusHours(1);
+
+
+
+                        //create body
+                        JsonObject newEvent = new JsonObject();
+                        newEvent.addProperty("Subject","Office 365 unified API discussion");
+                        newEvent.addProperty("Start",start.toString());
+                        newEvent.addProperty("End", end.toString());
+
+                        //create location
+                        JsonObject location = new JsonObject();
+                        location.addProperty("DisplayName","Bill's office");
+                        newEvent.add("Location", location);
+
+                        //create attendees array with one attendee
+                        //start with attendee
+                        JsonObject attendee = new JsonObject();
+                        attendee.addProperty("Type","Required");
+                        JsonObject emailaddress = new JsonObject();
+                        emailaddress.addProperty("Address","mara@fabrikam.com");
+                        attendee.add("EmailAddress", emailaddress);
+
+                        //now create attendees array
+                        JsonArray attendees = new JsonArray();
+                        attendees.add(attendee);
+                        newEvent.add("Attendees",attendees);
+
+                        //create email body
+                        JsonObject emailBody = new JsonObject();
+                        emailBody.addProperty("Content","Let's discuss the power of the Office 365 unified API.");
+                        emailBody.addProperty("ContentType","Text");
+                        newEvent.add("Body",emailBody);
+
+                        TypedString body = new TypedString(newEvent.toString()){
+                            @Override public String mimeType() {
+                                return "application/json";
+                            }
+                        };
+
+                        //Call service to POST the new event
+                        unifiedEventsService.postNewEvent(getVersion(), body, callback);
+                    }
+
+                },
+                 /*
+                 * PATCH update an event
+                 * HTTP PATCH
+                 * @see https://msdn.microsoft.com/office/office365/HowTo/office-365-unified-api-reference#msg_ref_entityType_Event
+                 */
+                new EventsSnippet<Void>(update_event) {
+
+                    @Override
+                    public void request(
+                            UnifiedEventsService unifiedEventsService,
+                            retrofit.Callback<Void> callback) {
+
+                        //TODO create JSON body of event update to make
+                        unifiedEventsService.getEvents(getVersion(), callback);
+                    }
+
+
+                },
+                 /*
+                 * DELETE delete an event
+                 * HTTP DELETE
+                 * @see https://msdn.microsoft.com/office/office365/HowTo/office-365-unified-api-reference#msg_ref_entityType_Event
+                 */
+                new EventsSnippet<Void>(delete_event) {
 
                     @Override
                     public void request(
