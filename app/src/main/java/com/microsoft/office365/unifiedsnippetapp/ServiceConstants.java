ef2423ec3fb4ec583bbf1a9541076f899feeb27c
unknown <davech@DAVECHL10.redmond.corp.microsoft.com>
2015-09-28 16:31:36 -0700

    added tokens

--- app/src/main/java/com/microsoft/office365/unifiedsnippetapp/ServiceConstants.java
+++ app/src/main/java/com/microsoft/office365/unifiedsnippetapp/ServiceConstants.java
@@ -6,8 +6,8 @@ package com.microsoft.office365.unifiedsnippetapp;
 public class ServiceConstants {
     public static final String AUTHENTICATION_RESOURCE_ID = "https://graph.microsoft.com";
     public static final String AUTHORITY_URL = "https://login.microsoftonline.com/common";
-    public static final String REDIRECT_URI = "http://localhost/unified";
-    public static final String CLIENT_ID = "6c7696f2-8db6-418d-ac57-da946c36b98c";
+    public static final String REDIRECT_URI = "<Your REDIRECT URI HERE>";
+    public static final String CLIENT_ID = "<Your CLIENT ID HERE>";
 }
 // *********************************************************
 //
