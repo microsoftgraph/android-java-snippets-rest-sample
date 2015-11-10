5a327c7d5b8e249148beacaf0eef0dccee021ce3
unknown <davech@DAVECHL10.redmond.corp.microsoft.com>
2015-11-09 19:02:44 -0800

    moved get contacts to beta status

--- app/src/main/java/com/microsoft/office365/unifiedsnippetapp/snippet/AbstractSnippet.java
+++ app/src/main/java/com/microsoft/office365/unifiedsnippetapp/snippet/AbstractSnippet.java
@@ -5,10 +5,12 @@ package com.microsoft.office365.unifiedsnippetapp.snippet;
 
 
 import com.microsoft.office365.unifiedapiservices.UnifiedContactService;
+import com.microsoft.office365.unifiedsnippetapp.R;
 import com.microsoft.office365.unifiedsnippetapp.application.SnippetApp;
 
 import retrofit.Callback;
 
+import static com.microsoft.office365.unifiedsnippetapp.R.string.beta;
 
 public abstract class AbstractSnippet<Service, Result> {
 
@@ -88,6 +90,11 @@ public abstract class AbstractSnippet<Service, Result> {
      */
     protected String getVersion() {
         return mO365Version;
+    }
+
+    public boolean isBeta() {
+        String betaString = SnippetApp.getApp().getString(beta);
+        return (mO365Version.equalsIgnoreCase(betaString));
     }
 
     public String getName() {
