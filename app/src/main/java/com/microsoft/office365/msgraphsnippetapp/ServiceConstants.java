56f0c261082caee11554ccd7b13418309f90f24f
David Chesnut <davech@microsoft.com>
2015-11-18 10:28:41 -0800

    restored tokens

--- app/src/main/java/com/microsoft/office365/msgraphsnippetapp/ServiceConstants.java
+++ app/src/main/java/com/microsoft/office365/msgraphsnippetapp/ServiceConstants.java
@@ -6,8 +6,8 @@ package com.microsoft.office365.msgraphsnippetapp;
 public class ServiceConstants {
     public static final String AUTHENTICATION_RESOURCE_ID = "https://graph.microsoft.com";
     public static final String AUTHORITY_URL = "https://login.microsoftonline.com/common";
-    public static final String REDIRECT_URI = "https://www.patsoldemo4.com";
-    public static final String CLIENT_ID = "e89a923e-7823-4a8f-ba21-0eb771c96a64";
+    public static final String REDIRECT_URI = "ENTER_YOUR_REDIRECT_URI";
+    public static final String CLIENT_ID = "ENTER_YOUR_CLIENT_ID";
 }
 // *********************************************************
 //
