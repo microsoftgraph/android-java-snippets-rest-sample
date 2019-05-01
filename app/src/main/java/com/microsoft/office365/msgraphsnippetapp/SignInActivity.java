155715700f2e1f08cfa63952959608dd2466c0d4
Irvine Sunday <v-irsund@microsoft.com>
2019-05-02 02:35:49 +0300

    Adding MSAL support
    Adding MSAL to SinginActivity

--- app/src/main/java/com/microsoft/office365/msgraphsnippetapp/SignInActivity.java
+++ app/src/main/java/com/microsoft/office365/msgraphsnippetapp/SignInActivity.java
@@ -6,15 +6,24 @@ package com.microsoft.office365.msgraphsnippetapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.microsoft.identity.client.AuthenticationCallback;
 import com.microsoft.identity.client.AuthenticationResult;
+import com.microsoft.identity.client.IAccount;
+import com.microsoft.identity.client.Logger;
+import com.microsoft.identity.client.exception.MsalClientException;
+import com.microsoft.identity.client.exception.MsalException;
+import com.microsoft.identity.client.exception.MsalServiceException;
+import com.microsoft.identity.client.exception.MsalUiRequiredException;
+import com.microsoft.office365.auth.AuthenticationManager;
 import com.microsoft.office365.msgraphsnippetapp.util.SharedPrefsUtil;
 
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
 
 import butterknife.ButterKnife;
@@ -33,6 +42,9 @@ import static com.microsoft.office365.msgraphsnippetapp.R.string.warning_clienti
 public class SignInActivity
         extends BaseActivity
         implements MSALAuthenticationCallback {
+
+    private boolean mEnablePiiLogging = false;
+    private static final String TAG = "SignInActivity";
 
     @InjectView(layout_diagnostics)
     protected View mDiagnosticsLayout;
@@ -72,8 +84,8 @@ public class SignInActivity
         // get the user display name
         final String userDisplayableId =
                 authenticationResult
-                        .getUserInfo()
-                        .getDisplayableId();
+                        .getAccount()
+                        .getUsername();
 
         // get the index of their '@' in the name (to determine domain)
         final int at = userDisplayableId.indexOf("@");
@@ -89,7 +101,7 @@ public class SignInActivity
     }
 
     @Override
-    public void onError(Exception e) {
+    public void onError(MsalException e) {
         e.printStackTrace();
 
         //Show the localized message supplied with the exception or
@@ -103,6 +115,24 @@ public class SignInActivity
             mDiagnosticsTxt.setText(msg);
             mDiagnosticsLayout.setVisibility(VISIBLE);
         }
+
+        if (e instanceof MsalClientException) {
+            // This means errors happened in the sdk itself, could be network, Json parse, etc. Check MsalError.java
+            // for detailed list of the errors.
+
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        } else if (e instanceof MsalServiceException) {
+            // This means something is wrong when the sdk is communication to the service, mostly likely it's the client
+            // configuration.
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        } else if (e instanceof MsalUiRequiredException) {
+            // This explicitly indicates that developer needs to prompt the user, it could be refresh token is expired, revoked
+            // or user changes the password; or it could be that no token was found in the token cache.
+            AuthenticationManager mgr = AuthenticationManager.getInstance();
+            mgr.callAcquireToken(SignInActivity.this, this);
+        }
     }
 
     private void warnBadClient() {
@@ -114,7 +144,60 @@ public class SignInActivity
 
     private void authenticate() throws IllegalArgumentException {
         validateOrganizationArgs();
-        mAuthenticationManager.connect(this);
+        connect();
+        AuthenticationManager.getInstance();
+    }
+
+    private void connect() {
+
+        // The sample app is having the PII enable setting on the MainActivity. Ideally, app should decide to enable Pii or not,
+        // if it's enabled, it should be  the setting when the application is onCreate.
+        if (mEnablePiiLogging) {
+            Logger.getInstance().setEnablePII(true);
+        } else {
+            Logger.getInstance().setEnablePII(false);
+        }
+
+        AuthenticationManager mgr = AuthenticationManager.getInstance();
+
+        /* Attempt to get a user and acquireTokenSilent
+         * If this fails we do an interactive request
+         */
+        List<IAccount> users = null;
+
+        try {
+            users = mgr.getPublicClient().getAccounts();
+
+            if (users != null && users.size() == 1) {
+                /* We have 1 user */
+                mUser = users.get(0);
+                mgr.callAcquireTokenSilent(
+                        mUser,
+                        true,
+                        this);
+            } else {
+                /* We have no user */
+
+                /* Let's do an interactive request */
+                mgr.callAcquireToken(
+                        this,
+                        this);
+            }
+        }
+
+        catch (IndexOutOfBoundsException e) {
+            Log.d(TAG, "User at this position does not exist: " + e.toString());
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+        } catch (IllegalStateException e) {
+            Log.d(TAG, "MSAL Exception Generated: " + e.toString());
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void validateOrganizationArgs() throws IllegalArgumentException {
@@ -127,4 +210,35 @@ public class SignInActivity
         startActivity(appLaunch);
     }
 
+    public IAccount mUser;
+
+    @Override
+    public void onError(Exception exception) {
+        Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCancel() {
+        Toast.makeText(this, "User cancelled the flow.", Toast.LENGTH_SHORT).show();
+    }
+    /**
+     * Handles redirect response from https://login.microsoftonline.com/common and
+     * notifies the MSAL library that the user has completed the authentication
+     * dialog
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (AuthenticationManager
+                .getInstance()
+                .getPublicClient() != null) {
+            AuthenticationManager
+                    .getInstance()
+                    .getPublicClient()
+                    .handleInteractiveRequestRedirect(requestCode, resultCode, data);
+        }
+    }
 }
