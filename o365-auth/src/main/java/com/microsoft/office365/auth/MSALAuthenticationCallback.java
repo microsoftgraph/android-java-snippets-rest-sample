1934d80c74838f167b07182ef5453142f3b5ad15
Irvine Sunday <v-irsund@microsoft.com>
2019-05-02 21:17:57 +0300

    Implementing the authentication callback

--- o365-auth/src/main/java/com/microsoft/office365/auth/MSALAuthenticationCallback.java
+++ o365-auth/src/main/java/com/microsoft/office365/auth/MSALAuthenticationCallback.java
@@ -1,4 +1,11 @@
 package com.microsoft.office365.auth;
 
+import com.microsoft.identity.client.AuthenticationResult;
+import com.microsoft.identity.client.exception.MsalException;
+
 public interface MSALAuthenticationCallback {
+    void onSuccess(AuthenticationResult authenticationResult);
+    void onError(MsalException exception);
+    void onError(Exception exception);
+    void onCancel();
 }
