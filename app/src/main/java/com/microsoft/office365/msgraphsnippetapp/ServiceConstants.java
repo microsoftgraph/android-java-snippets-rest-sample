e568e1c8270f7209829a4e02b2a1390263caab44
Irvine Sunday <v-irsund@microsoft.com>
2019-05-07 13:09:54 +0300

    Updating permission scopes
    Fixes bug that caused GET /groups snippets to fail to get data

--- app/src/main/java/com/microsoft/office365/msgraphsnippetapp/ServiceConstants.java
+++ app/src/main/java/com/microsoft/office365/msgraphsnippetapp/ServiceConstants.java
@@ -8,5 +8,5 @@ public class ServiceConstants {
     public static final String AUTHENTICATION_RESOURCE_ID = "https://graph.microsoft.com";
     public static final String AUTHORITY_URL = "https://login.microsoftonline.com/common";
     public static final String CLIENT_ID = "ENTER_YOUR_CLIENT_ID";
-    public static final String[] SCOPES = {"openid", "Directory.Read.All","Calendars.ReadWrite","Files.ReadWrite","Mail.ReadWrite","Mail.Send","User.ReadBasic.All", "Group.Read.All", "profile"};
+    public static final String[] SCOPES = {"openid", "Directory.ReadWrite.All","Calendars.ReadWrite","Files.ReadWrite","Mail.ReadWrite","Mail.Send","User.ReadBasic.All", "Group.ReadWrite.All", "profile"};
 }
